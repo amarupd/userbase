f479b1a833b2d1fcb0fc591c9168eef8737b0422
amar dutt upadhyay <amarduttupadhyay@gmail.com>
2023-02-06 11:24:38 +0530

    commiy

--- controller/logincont.js
+++ controller/logincont.js
@@ -1,5 +1,5 @@
 const db = require("../model")
-const Timestamp = db.timestamps;
+const loginUser = db.timestamps;
 const sequelize = require('../sequelizetemplate')
 
 
@@ -12,64 +12,62 @@ const punchin = async (req, res) => {
     // const id = req.body.id
     
     let info = {
-        date: req.body.date,
-        punchIN: sequelize.literal('CURRENT_TIMESTAMP'),
-        punchOUT: req.body.punchOUT,
-        empID: req.body.empID,
-        missed_punch: req.body.missed_punch
+        userID: req.body.userID,
+        password: req.body.password,
+        timeStamp: req.body.timeStamp
     }
 
-    const emp = await Timestamp.create(info)
+    const emp = await loginUser.create(info)
     // await Timestamp.update({ punchIN: sequelize.literal('CURRENT_TIMESTAMP') })
     res.status(200).send(emp)
 }
 
 /********************************************************************************************************************************************** */
 
-const punchout = async (req, res) => {
-    const id = req.body.empID
-    const date = await sequelize.query(`SELECT CURDATE() as today`,
-    {
-        type: QueryTypes.SELECT
-    });
-    let mapArray = date.map(item => item.today);
-    const st = mapArray.toString();
-    console.log(st);
-    await Timestamp.update({ punchOUT: sequelize.literal('CURRENT_TIMESTAMP') }, { where: { empID: id, date: st } })
-    // await Timestamp.update({ total_working_hour: sequelize.query(`SELECT TIMEDIFF(punchOUT, punchIN) from mymaster11.timestamp11s`) }, { where: { empID: id } })
-    const punchi = await sequelize.query(`SELECT punchIN FROM timestamps WHERE empID =${id}`,
-        {
-            type: QueryTypes.SELECT
-        });
-    const puncho = await sequelize.query(`SELECT punchOUT FROM timestamps WHERE empID =${id}`,
-        {
-            type: QueryTypes.SELECT
-        });
-    const punchou = await sequelize.query(`SELECT TIMEDIFF(punchOUT, punchIN) as totaltime FROM timestamps WHERE empID =${id} `,
-        {
-            type: QueryTypes.SELECT
-        });
-    res.status(200).send({ message: "thank you", data: puncho })
-    let mappedArray = punchou.map(item => item.totaltime);
-    const str = mappedArray.toString();
+// const punchout = async (req, res) => {
+//     const id = req.body.empID
+//     const date = await sequelize.query(`SELECT CURDATE() as today`,
+//     {
+//         type: QueryTypes.SELECT
+//     });
+//     let mapArray = date.map(item => item.today);
+//     const st = mapArray.toString();
+//     console.log(st);
+//     await Timestamp.update({ punchOUT: sequelize.literal('CURRENT_TIMESTAMP') }, { where: { empID: id, date: st } })
+//     // await Timestamp.update({ total_working_hour: sequelize.query(`SELECT TIMEDIFF(punchOUT, punchIN) from mymaster11.timestamp11s`) }, { where: { empID: id } })
+//     const punchi = await sequelize.query(`SELECT punchIN FROM timestamps WHERE empID =${id}`,
+//         {
+//             type: QueryTypes.SELECT
+//         });
+//     const puncho = await sequelize.query(`SELECT punchOUT FROM timestamps WHERE empID =${id}`,
+//         {
+//             type: QueryTypes.SELECT
+//         });
+//     const punchou = await sequelize.query(`SELECT TIMEDIFF(punchOUT, punchIN) as totaltime FROM timestamps WHERE empID =${id} `,
+//         {
+//             type: QueryTypes.SELECT
+//         });
+//     res.status(200).send({ message: "thank you", data: puncho })
+//     let mappedArray = punchou.map(item => item.totaltime);
+//     const str = mappedArray.toString();
 
-    console.log(str);
+//     console.log(str);
 
-    if (punchi != 'NULL' || puncho != 'NULL') {
-        await Timestamp.update({ missed_punch: 0 }, { where: { empID: id } })  /// yaha update karna h dimaaag lagao yhi pe date wala karna h
-    } else {
-        await Timestamp.update({ missed_punch: 1 }, { where: { empID: id } }) /// yaha update karna h dimaaag lagao yhi pe date wala karna h
-    }
+//     if (punchi != 'NULL' || puncho != 'NULL') {
+//         await Timestamp.update({ missed_punch: 0 }, { where: { empID: id } })  /// yaha update karna h dimaaag lagao yhi pe date wala karna h
+//     } else {
+//         await Timestamp.update({ missed_punch: 1 }, { where: { empID: id } }) /// yaha update karna h dimaaag lagao yhi pe date wala karna h
+//     }
 
 
-    await Timestamp.update({ total_working_hour: str }, { where: { empID: id } })
+//     await Timestamp.update({ total_working_hour: str }, { where: { empID: id } })
 
-}
+// }
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 const details = async (req, res) => {
-    let times = await Timestamp.findAll({})
+    let times = await loginUser.findAll({})
     res.status(200).send(times)
 }
 
