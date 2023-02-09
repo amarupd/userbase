b8ce0ce66ef3e3b228789d15bf99316246bad07f
amar dutt upadhyay <amarduttupadhyay@gmail.com>
2023-02-09 15:44:24 +0530

    coommmmittttt

--- controller/registrationcont.js
+++ controller/registrationcont.js
@@ -1,6 +1,7 @@
 const db = require("../models")
 const addUser = db.registrations;
 const loginU = db.logins;
+const Joi=require('joi')
 // const sequelize = require('../sequelizetemplate')
 
 
@@ -11,6 +12,20 @@ const loginU = db.logins;
 /********************************************************************************************** */
 const adduser = async (req, res) => {
 
+    // const signupSchema=Joi.object({
+    //     email:Joi.string().email().required()
+    //     // password:Joi.string().min(5).max(15).required(),
+    // })
+    // const {error, value}=signupSchema.validate(req.body.email,
+    //     {
+    //         abortEarly:false
+    //     })
+    // if(error){
+    //     res.status(400).send({
+    //         message:'invalid email format',
+    //         data:value
+    //     })
+    // }
     let info = {
         fullname: req.body.fullname,
         email: req.body.email,
@@ -24,9 +39,30 @@ const adduser = async (req, res) => {
         password: req.body.password,
         timeStamp: req.body.timeStamp
     }
+    const email=req.body.email
+    const mobile_number=req.body.mobile_number
     const password = req.body.password;
     const confirm_password = req.body.confirm_password;
 
+    const emailId=await addUser.findOne({
+        where: { email: email }
+    })
+    const mobile=await addUser.findOne({
+        where: { mobile_number: mobile_number }
+    })
+if(emailId!='NULL')
+{
+  res.status(400).send({message:`${email} is already registered.`})
+}
+else if(mobile_number!='NULL')
+{
+  res.status(400).send({message:`${mobile_number} is already registered.`})
+}
+else if(mobile_number!='NULL' || emailId!='NULL')
+{
+  res.status(400).send({message:`${mobile_number} is already registered.`})
+}
+else{
     if (password == confirm_password) {
         const emp = await addUser.create(info)
         res.status(200).send({message:"user added succesfully",data:emp})
@@ -35,6 +71,10 @@ const adduser = async (req, res) => {
     else{
         res.status(400).send({message:"password didnt match"})
     }
+}
+    
+
+    
 }
 
 
