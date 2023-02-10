94a50d7a15396c0d6c03504bc94c86aa4641c583
amar dutt upadhyay <amarduttupadhyay@gmail.com>
2023-02-10 14:39:45 +0530

    commit

--- controller/registrationcont.js
+++ controller/registrationcont.js
@@ -2,7 +2,7 @@ const db = require("../models")
 const addUser = db.registrations;
 const { validateSignup } = require('../validator')
 const loginU = db.logins;
-const bcrypt=require('bcryptjs')
+const bcrypt = require('bcryptjs')
 const Joi = require('joi')
 // const sequelize = require('../sequelizetemplate')
 
@@ -14,62 +14,64 @@ const Joi = require('joi')
 /********************************************************************************************** */
 const adduser = async (req, res) => {
 
-    const {error,value}=validateSignup(req.body)
-    if(error){
+    const { error, value } = validateSignup(req.body)
+    if (error) {
         console.log(error);
         res.send(error.details)
     }
-    else{
+    else {
 
-        const passcode=req.body.password
-        const compPass=req.body.confirm_password
-            const passwordHash=await bcrypt.hash(passcode,10)
-            console.log(passwordHash);
-
-
+        const passcode = req.body.password
+        const compPass = req.body.confirm_password
+        const passwordHash = await bcrypt.hash(passcode, 10)
+        console.log(passwordHash);
+        const compPasswordHash = await bcrypt.compare(compPass, passwordHash)
+        console.log(compPasswordHash);
+        const compPassswordHash = await bcrypt.hash(compPass, 10)
+        console.log(compPasswordHash);
         let info = {
             fullname: req.body.fullname,
             email: req.body.email,
             mobile_number: req.body.mobile_number,
             userID: req.body.userID,
-            password: passcode,
-            confirm_password: compPass
+            password: passwordHash,
+            confirm_password: passwordHash
         }
         let login = {
             userID: req.body.userID,
-            password: req.body.password,
+            password: passwordHash,
             timeStamp: req.body.timeStamp
         }
-        // const email = req.body.email
-        // const mobile_number = req.body.mobile_number
-        const password = req.body.password;
-        const confirm_password = req.body.confirm_password;
-    
+        const email = req.body.email
+        const mobile_number = req.body.mobile_number
+        // const password = req.body.password;
+        // const confirm_password = req.body.confirm_password;
+
         const emailId = await addUser.findOne({
             where: { email: email }
         });
         const mobile = await addUser.findOne({
             where: { mobile_number: mobile_number }
         });
-        if (emailId != 'NULL') {
+        if (emailId == 'NULL') {
             res.status(400).send({ message: `${email} is already registered.` })
         }
-        else if (mobile != 'NULL') {
+        else if (mobile == 'NULL') {
             res.status(400).send({ message: `${mobile} is already registered.` })
         }
         else {
-        if (password == confirm_password) {
-            const emp = await addUser.create(info)
-            res.status(200).send({ message: "user added succesfully", data: emp })
-            const log = await loginU.create(login)
-        }
-        else {
-            res.status(400).send({ message: "password didnt match" })
+            if (compPasswordHash) {
+                const emp = await addUser.create(info)
+                res.status(200).send({ message: "user added succesfully", data: emp })
+                const log = await loginU.create(login)
+            }
+            else {
+                res.status(400).send({ message: "password didnt match" })
+            }
         }
     }
-}
     console.log(value);
-    
+
 
 
 }
