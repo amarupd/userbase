c2c63ed76383bbd1188176064be057465c81e72f
amar dutt upadhyay <amarduttupadhyay@gmail.com>
2023-02-15 15:16:46 +0530

    commit

--- controller/otplogincont.js
+++ controller/otplogincont.js
@@ -2,7 +2,7 @@ const db = require("../models")
 const addUser = db.registrations;
 const sequelize = require('../sequelizetempelate')
 var CryptoJS = require("crypto-js")
-const axios=require('axios')
+const axios = require('axios')
 
 const { QueryTypes } = require('sequelize');
 
@@ -11,9 +11,10 @@ const { QueryTypes } = require('sequelize');
 //     res.status(200).send(times)
 // }
 
-var seq = (Math.floor(Math.random() * 10000) + 10000).toString().substring(1);
+//  var seq = (Math.floor(Math.random() * 10000) + 10000).toString().substring(1);
 
 const otpLogin = async (req, res) => {
+     var seq = (Math.floor(Math.random() * 10000) + 10000).toString().substring(1);
     const mobile_number = req.body.mobile_number
     const user = await sequelize.query(`SELECT mobile_number FROM registrations WHERE mobile_number ='${mobile_number}'`,
         {
@@ -22,22 +23,29 @@ const otpLogin = async (req, res) => {
     let uID = user.map(item => item.mobile_number);
     const str = uID.toString();
 
-    console.log(str); 
+    console.log(str);
 
     // const compPasswordHash = await bcrypt.compare(password,passwordHash)
     // console.log(compPasswordHash);
 
     if (mobile_number == str) {
-        const url = `https://bulksms.analyticsmantra.com/sendsms/sendsms.php?username=SUNSURRYA&password=tech321&type=TEXT&sender=MMSTER&mobile=${mobile_number}&message=Use%20${seq}%20as%20OTP%20to%20login%20into%20MyMaster11&PEID=1201161650796863916&HeaderId=1205161650964871534&templateId=1207161736910206136`;
-        let response = await axios.get(url);
-        console.log(response);
+        // const url = `https://bulksms.analyticsmantra.com/sendsms/sendsms.php?username=SUNSURRYA&password=tech321&type=TEXT&sender=MMSTER&mobile=${mobile_number}&message=Use%20${seq}%20as%20OTP%20to%20login%20into%20MyMaster11&PEID=1201161650796863916&HeaderId=1205161650964871534&templateId=1207161736910206136`;
+        // let response = await axios.get(url);
+        // console.log(response);
         var ciphertext = CryptoJS.AES.encrypt(`${mobile_number}.${seq}`, `${seq}`).toString();
         console.log(ciphertext)
-        res.status(200).send({ message:'otp sent succesfull',data:ciphertext})
+        res.status(200).send({ message: 'otp sent succesfull', data: ciphertext })
     }
+    else{
+        res.status(200).send(`${mobile_number} is not registered with us`)
+    }
+    console.log(seq);
+    // return seq;
 }
 
 const otpverify = async (req, res) => {
+    // const seq=otpLogin();
+    console.log(seq)
     console.log(`value of otp is ${seq}`);
     const mobileno = req.body.mobile_number
     const OTP = req.body.otp
@@ -48,7 +56,7 @@ const otpverify = async (req, res) => {
     const m = string[0];
     const otp = string[1];
     console.log(m, otp);
-    
+
     if (m == mobileno && OTP == otp) {
         // const { value } = validateSignup(pass)
         // const passwordHash = await bcrypt.hash(pass, 10)
