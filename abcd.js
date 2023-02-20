13596e59fcbb5d3abc47e010b73fddd165d571b5
amar dutt upadhyay <amarduttupadhyay@gmail.com>
2023-02-20 11:55:55 +0530

    xx

--- abcd.js
+++ abcd.js
@@ -1,14 +1,15 @@
 const http = require('http');
 
-// const hostname = '13.126.177.194';
+const hostname = 'ec2-13-232-179-88.ap-south-1.compute.amazonaws.com';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-	res.statusCode = 200;
-  	res.setHeader('Content-Type', 'text/plain');
-  	res.end('This is the Main App!\n');
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('This is the Admin Side!\n');
 });
 
 server.listen(port, () => {
-  	console.log(`Server running at http://localhost:${port}/`);
+        console.log(`Server running at http://${hostname}:${port}/`);
 });
+
